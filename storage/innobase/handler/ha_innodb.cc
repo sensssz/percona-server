c6d3e7ca0466ffe2b1cea2c359b77cf3f7ca43c2
sensssz <hjmsens@gmail.com>
2016-11-22 16:35:12 -0500

    Error fix.

--- storage/innobase/handler/ha_innodb.cc
+++ storage/innobase/handler/ha_innodb.cc
@@ -1359,7 +1359,7 @@ thd_is_replication_slave_thread(
 /*============================*/
 	THD*	thd)	/*!< in: thread handle */
 {
-	return((ibool) thd_slave_thread(thd));
+	return(thd && (ibool) thd_slave_thread(thd));
 }
 
 /******************************************************************//**
