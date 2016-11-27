533b3d2d6c1f2848723ed21f2509a51fe3e03eed
sensssz <hjmsen@gmail.com>
2016-11-27 15:46:30 -0500

    Remove build folder.

--- build/5.6-debug/mysql-test/mysql-test-run.pl
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/perl
-# Call mtr in out-of-source build
-$ENV{MTR_BINDIR} = '/home/jiamin/percona-server/build/5.6-debug';
-chdir('/home/jiamin/percona-server/mysql-test');
-exit(system($^X, '/home/jiamin/percona-server/mysql-test/mysql-test-run.pl', @ARGV) >> 8);
