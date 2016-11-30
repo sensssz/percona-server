55da7646926286247eb803a1fddf99a83475f75f
sensssz <hjmsens@gmail.com>
2016-11-30 17:31:01 -0500

    Add more statistics.

--- storage/innobase/lock/lock0lock.cc
+++ storage/innobase/lock/lock0lock.cc
@@ -78,6 +78,8 @@ ulong innodb_lock_schedule_algorithm = INNODB_LOCK_SCHEDULE_ALGORITHM_VATS;
 
 double total_schedule = 0;
 double has_diff_schedule = 0;
+double schedule_total = 0;
+double schedule_diff = 0;
 time_t last_update = 0;
 
 /* An explicit record lock affects both the record and the gap before it.
@@ -2822,6 +2824,8 @@ lock_rec_dequeue_from_page(
 //                fprintf(stderr, "Set diff: %d\n", diff);
                 total_schedule++;
                 has_diff_schedule += diff > 0;
+                schedule_total += fcfs_granted.size() + vats_granted.size() - 2 * granted_locks.size();
+                schedule_diff += diff;
             }
 //            if (wait_locks.size() > 0) {
 //                for (i = 0; i < wait_locks.size(); ++i) {
