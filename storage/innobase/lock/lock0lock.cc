333387e6261892631b01b20c53ae97d7d95a46b8
sensssz <hjmsens@gmail.com>
2016-11-29 21:16:38 -0500

    Error fix.

--- storage/innobase/lock/lock0lock.cc
+++ storage/innobase/lock/lock0lock.cc
@@ -2772,7 +2772,7 @@ lock_rec_dequeue_from_page(
                     wait_locks.push_back(lock);
                 }
             }
-            std::sort(wait_locks.begin, wait_locks.end(), has_higher_priority);
+            std::sort(wait_locks.begin(), wait_locks.end(), has_higher_priority);
             for (auto lock : wait_locks) {
                 if (!lock_rec_has_to_wait_granted(lock, granted_locks)) {
                     lock_grant(lock, false);
