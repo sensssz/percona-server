23eb4c417a10c85c1eae6611c9545fe3d9af093f
sensssz <hjmsens@gmail.com>
2016-11-30 13:49:44 -0500

    Fix printf error.

--- storage/innobase/srv/srv0start.cc
+++ storage/innobase/srv/srv0start.cc
@@ -3147,7 +3147,7 @@ innobase_shutdown_for_mysql(void)
 	srv_was_started = FALSE;
 	srv_start_has_been_called = FALSE;
 
-    fprintf(stderr, "Schedule statistics: %ld,%ld,%ld\n", total_schedule, has_diff_schedule, has_diff_schedule / total_schedule);
+    fprintf(stderr, "Schedule statistics: %lf,%lf,%lf\n", total_schedule, has_diff_schedule, has_diff_schedule / total_schedule);
 
 	return(DB_SUCCESS);
 }
